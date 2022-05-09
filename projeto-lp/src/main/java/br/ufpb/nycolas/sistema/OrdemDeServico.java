a735cdd9f2138bc3232620a7b597ee32a181d54b
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-05-09 11:38:10 -0300

    Getters e setters da classe OrdemDeSercivo prontos

--- projeto-lp/src/main/java/br/ufpb/nycolas/sistema/OrdemDeServico.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/sistema/OrdemDeServico.java
@@ -1,5 +1,56 @@
 package br.ufpb.nycolas.sistema;
 
 public class OrdemDeServico {
-    
+
+    private int id;
+    private String status;
+    private String descricaoProblema;
+
+    /**
+     * Default constructor
+     */
+    public OrdemDeServico() {
+    }
+
+    /**
+     * @return Retorna o id dessa OS
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * @param novoId Novo id para essa OS
+     */
+    public void setId(int novoId) {
+        this.id = novoId;
+    }
+
+    /**
+     * @return retorna o status desta os
+     */
+    public String getStatus() {
+        return this.status;
+    }
+
+    /**
+     * @param novoStatus Novo status da OS
+     */
+    public void setStatus(String novoStatus) {
+        this.status = novoStatus;
+    }
+
+    /**
+     * @return Retorna a descrição do problema descrito
+     */
+    public String getDescricaoProblema() {
+        return this.descricaoProblema;
+    }
+
+    /**
+     * @param novaDescricao Nova descrição para está OS
+     */
+    public void setDrescricaoProblema(String novaDescricao) {
+        this.descricaoProblema = novaDescricao;
+    }
 }
