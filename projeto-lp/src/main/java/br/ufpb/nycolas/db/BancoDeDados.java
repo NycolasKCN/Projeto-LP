274b9cc34fa83bccb7a495bcca5a8a87c63b33c7
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-05-09 10:48:39 -0300

    Muita coisa já feita

---
+++ projeto-lp/src/main/java/br/ufpb/nycolas/db/BancoDeDados.java
@@ -0,0 +1,47 @@
+package br.ufpb.nycolas.db;
+
+import br.ufpb.nycolas.sistema.*;
+import java.util.ArrayList;
+
+public class BancoDeDados {
+
+    private ArrayList<OrdemDeServico> ordensDeServico;
+    private ArrayList<Funcionario> funcionarios;
+    private String caminhoArquivo;
+
+    /**
+     * 
+     */
+    public BancoDeDados() {
+    }
+
+    public void consultarAparelhoPorNome() {
+        // TODO implement here
+    }
+
+    public void cadastrarNovoAparelho() {
+        // TODO implement here
+    }
+
+    public ArrayList<Aparelho> consultarTodosAparelhos() {
+        // TODO implement here
+        return null;
+    }
+
+    public void registrarNovaOS() {
+        // TODO implement here
+    }
+
+    public void registrarNovoFuncionario() {
+        // TODO implement here
+    }
+
+    public static void lerAquivo() {
+        // TODO implement here
+    }
+
+    public static boolean AquivoExiste() {
+        // TODO implement here
+        return false;
+    }
+}
---
+++ projeto-lp/src/main/java/br/ufpb/nycolas/sistema/Funcionario.java
@@ -0,0 +1,5 @@
+package br.ufpb.nycolas.sistema;
+
+public class Funcionario {
+    
+}
