51911a380942b548d8db49e4cda2fc7f78b44299
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-05-09 11:08:16 -0300

    Estrutura do banco de dados feita e Construção do Aparelho

--- projeto-lp/src/main/java/br/ufpb/nycolas/sistema/Aparelho.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/sistema/Aparelho.java
@@ -1,5 +1,70 @@
 package br.ufpb.nycolas.sistema;
 
 public class Aparelho {
+    private int id;
+    private String marca;
+    private String modelo;
+    private String descricao;
+
+    /**
+     * Default constructor
+     */
+    public Aparelho() {
+    }
+
+    /**
+     * @return retorna o id do aparelho
+     */
+    public int getId() {
+        return this.id;
+    }
     
+    /**
+     * @param novoId Novo id do aparelho
+     */
+    public void setId(int novoId) {
+        this.id = novoId;
+    }
+
+    /**
+     * @return retorna a marca do aparelho
+     */
+    public String getMarca() {
+        return this.marca;
+    }
+
+    /**
+     * @param novaMarca Nova marca do aparelho
+     */
+    public void setMarca(String novaMarca) {
+        this.marca = novaMarca;
+    }
+
+    /**
+     * @return Retorna o modelo do aparelho
+     */
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    /**
+     * @param novoModelo Novo modelo do aparelho
+     */
+    public void setModelo(String novoModelo) {
+        this.modelo = novoModelo;
+    }
+
+    /**
+     * @return Retorna a descrição do aparelho
+     */
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    /**
+     * @param novaDesc Nova descrição do aparelho
+     */
+    public void setDescricao(String novaDesc) {
+        this.descricao = novaDesc;
+    }
 }
