a15798598c402b80f4d89dae2f43d62a6db58dd3
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-05-12 18:44:21 -0300

    .

--- projeto-lp/src/main/java/br/ufpb/nycolas/db/LerBancoDeDados.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/db/LerBancoDeDados.java
@@ -1,20 +1,29 @@
 package br.ufpb.nycolas.db;
-// import java.io.Reader;
-// import java.nio.file.Files;
-// import java.nio.file.Paths;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
 
 public class LerBancoDeDados {
-    private String caminhoArquivoAparelhos = "/dados/aparelhos.csv";
-    private String caminhoArquivoFuncionarios = "/dados/funcionarios.csv";
-    private String caminhoArquivoOs = "/dados/ordensDeServico.csv";
-
-    public void lerAquivos(String caminhoArquivo) {
+    public void lerAquivos() {
         // TODO
     }
 
-    public boolean arquivoExiste() {
-        // TODO
+    /**
+     * Recebe um caminho de um arquivo e verifica se este arquivo existe
+     * 
+     * @param caminhoAquivo o caminho completo de onde o arquivo se encontra
+     * @return Retorna verdadeiro caso exista e falso caso não exista
+     */
+    public boolean arquivoExiste(String caminhoAquivo) {
         boolean existe = false;
+        File fileAparelhos = new File(caminhoAquivo);
+
+        if (fileAparelhos.exists() && !fileAparelhos.isDirectory()) {
+            existe = true;
+        }
         return existe;
     }
 
--- projeto-lp/src/test/java/br/ufpb/nycolas/db/testLerBancoDeDados.java
+++ projeto-lp/src/test/java/br/ufpb/nycolas/db/testLerBancoDeDados.java
@@ -1,5 +1,7 @@
 package br.ufpb.nycolas.db;
 
 public class testLerBancoDeDados {
-    
+    public static void main(String[] args) {
+        LerBancoDeDados lBd = new LerBancoDeDados();
+    }
 }
