7a8f1923a96a346841785a6bd96fae5dbae63bdc
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-05-31 20:01:50 -0300

    .

--- projeto-lp/src/main/java/br/ufpb/nycolas/ui/UiTerminal.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/ui/UiTerminal.java
@@ -2,17 +2,26 @@ package br.ufpb.nycolas.ui;
 
 import java.util.Scanner;
 
-public class UserInterface {
+
+public class UiTerminal {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("===== Assistência técnica =====");
         System.out.println("Menu: ");
         System.out.println("1. Cadastros");
         System.out.println("2. Consultas");
         System.out.println("3. Gerenciar");
-        System.out.print("Operação: ");
-        Scanner scan = new Scanner(System.in);
+        System.out.println("999. sair");
+        boolean continuar = true;
+        while (continuar) {
+            System.out.print("Operação: ");
+            String operacao = scan.nextLine();
 
-        String operacao = scan.nextLine();
+            if (operacao.equals("999")) {
+                break;
+            }
+        }
 
         scan.close();
     }
