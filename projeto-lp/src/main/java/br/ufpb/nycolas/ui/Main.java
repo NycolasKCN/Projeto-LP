d69c30f68017cca6ec3c8b7a281c730fe59b8380
Nycolas Kevin Costa Nascimento <nycolas.costa@dcx.ufpb.br>
2022-06-09 16:41:40 -0300

    .

--- projeto-lp/src/main/java/br/ufpb/nycolas/ui/Main.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/ui/Main.java
@@ -1,5 +1,7 @@
 package br.ufpb.nycolas.ui;
 
+// import br.ufpb.nycolas.ui.gui.MenuGui;
+
 public class Main {
     public static void main(String[] args) {
         new MenuTerminal();
--- projeto-lp/src/main/java/br/ufpb/nycolas/ui/MenuTerminal.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/ui/MenuTerminal.java
@@ -1,19 +1,21 @@
 package br.ufpb.nycolas.ui;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MenuTerminal implements Menu {
 
-    public MenuTerminal(){
+    public MenuTerminal() {
         this.init();
     }
-    
+
     public void init() {
         Scanner scan = new Scanner(System.in);
 
         boolean continuar = true;
 
         while (continuar) {
+            clean();
             System.out.println("===== Assistência técnica =====");
             System.out.println("Menu: ");
             System.out.println("1. Cadastros");
@@ -29,6 +31,7 @@ public class MenuTerminal implements Menu {
             } else if (operacao.equals("1")) {
 
                 while (true) {
+                    clean();
                     System.out.println("===== Cadastros =====");
                     System.out.println("1. Cadastrar Aparelho");
                     System.out.println("2. Cadastrar Funcionario");
@@ -47,13 +50,15 @@ public class MenuTerminal implements Menu {
 
                     } else {
                         System.out.println("Operação invalida! tente novamente.");
+                        espere();
                     }
                 } // fim do loop: menu de cadastros
 
             } else if (operacao.equals("2")) {
 
                 while (true) {
-                    System.out.println("===== Consultar =====");
+                    clean();
+                    System.out.println("===== Consultas =====");
                     System.out.println("1. Consultar Aparelho");
                     System.out.println("2. Consultar Funcionario");
                     System.out.println("3. Consultar Ordem de serviço");
@@ -71,12 +76,14 @@ public class MenuTerminal implements Menu {
 
                     } else {
                         System.out.println("Operação invalida! tente novamente.");
+                        espere();
                     }
                 } // fim do loop: menu consultas
 
             } else if (operacao.equals("3")) {
 
                 while (true) {
+                    clean();
                     System.out.println("===== Apagar =====");
                     System.out.println("1. Apagar Aparelho");
                     System.out.println("2. Apagar Funcionario");
@@ -95,14 +102,35 @@ public class MenuTerminal implements Menu {
 
                     } else {
                         System.out.println("Operação invalida! tente novamente.");
+                        espere();
                     }
                 } // fim do loop: menu apagar
             } else {
                 System.out.println("Operação invalida, Tente novamente!");
+                espere();
             }
         } // fim do loop: menu principal
 
         System.out.println("Fim do programa!");
         scan.close();
+    }
+
+    public void clean() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void espere() {
+        try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 }
