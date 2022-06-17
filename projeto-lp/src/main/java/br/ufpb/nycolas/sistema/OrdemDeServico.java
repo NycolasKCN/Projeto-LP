bec215124dc43800bd74178add2e74fe9a688c1f
Nycolas Kevin <nycolas.costa@dcx.ufpb.br>
2022-06-17 16:08:12 -0300

    Documentação OrdemDeServico.java

--- projeto-lp/src/main/java/br/ufpb/nycolas/sistema/OrdemDeServico.java
+++ projeto-lp/src/main/java/br/ufpb/nycolas/sistema/OrdemDeServico.java
@@ -1,15 +1,27 @@
 package br.ufpb.nycolas.sistema;
 
+/**
+ * Representação de uma ordem de serviço
+ * 
+ * @author Nycolas Kevin
+ */
 public class OrdemDeServico {
-
-    // TODO documentar todas as funções
-
     private String id;
     private String status;
     private String descricaoProblema;
     private Aparelho aparelhoVinculado;
     private Funcionario funcionarioResponsavel;
 
+    /**
+     * Constroi uma nova ordem de serviço
+     * 
+     * @param id                     id atribuida para está os
+     * @param status                 estado de progresso
+     * @param descricao              descrição do aparelho
+     * @param aparelhoVinculado      Aparelho vinculado a está ordem de serviço
+     * @param funcionarioResponsavel Funcionario que atendeu o cliente ou que
+     *                               recebeu o aparelho
+     */
     public OrdemDeServico(String id, String status, String descricao, Aparelho aparelhoVinculado,
             Funcionario funcionarioResponsavel) {
         this.id = id;
@@ -20,42 +32,24 @@ public class OrdemDeServico {
     }
 
     /**
-     * Construtor padrão, nenhum parametro
+     * Construtor padrão
      */
     public OrdemDeServico() {
         this("0", "vazio", "vazio", new Aparelho(), new Funcionario());
     }
 
     /**
-     * Construtor padrão, nenhum parametro
+     * Constroi uma ordem de serviço sem Id
+     * 
+     * @param status                 estado de progresso
+     * @param descricao              descrição do aparelho
+     * @param aparelhoVinculado      Aparelho vinculado a está ordem de serviço
+     * @param funcionarioResponsavel Funcionario que atendeu o cliente ou que
+     *                               recebeu o aparelho
      */
-    public OrdemDeServico(String status, String descricao, Aparelho ap, Funcionario fu) {
-        this("0", status, descricao, ap, fu);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        OrdemDeServico other = (OrdemDeServico) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public OrdemDeServico(String status, String descricao, Aparelho aparelhoVinculado,
+            Funcionario funcionarioResponsavel) {
+        this("0", status, descricao, aparelhoVinculado, funcionarioResponsavel);
     }
 
     /**
@@ -137,5 +131,30 @@ public class OrdemDeServico {
      */
     public void setFuncionarioResponsavel(Funcionario funcionarioResponsavel) {
         this.funcionarioResponsavel = funcionarioResponsavel;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrdemDeServico other = (OrdemDeServico) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
